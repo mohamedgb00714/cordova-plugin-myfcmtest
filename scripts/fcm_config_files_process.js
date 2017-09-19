4bbdfd3f2cf7f3ac49de541aeec4c2ffc8d1767f
msaid mohamed <mohamedgb00714@gmail.com>
2017-09-19 07:52:26 +0100

    try latest version of com.google.gms:google-services

--- scripts/fcm_config_files_process.js
+++ scripts/fcm_config_files_process.js
@@ -50,7 +50,7 @@ if (directoryExists("platforms/ios")) {
 }
 
 if (directoryExists("platforms/android")) {
-	var path = "/www/google-services.json";
+	var path = "assets/www/google-services.json";
 
     if (fileExists( path )) {
       try {
